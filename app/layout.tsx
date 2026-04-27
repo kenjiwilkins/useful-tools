1e2ebfcf00774599bb9bfa3f64cf559088cdbfe1
Michael Kenji Wilkins <kenji.wilkins@gmail.com>
2026-04-27 15:26:26 +0900

    use LINE Seed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { LINE_Seed_JP, Geist_Mono } from "next/font/google"
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lineSeedJp = LINE_Seed_JP({
+  preload: true,
+  weight: ["100", "400", "700", "800"],
   subsets: ["latin"],
+  variable: "--font-line-seed-jp",
 });
 
 const geistMono = Geist_Mono({
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${lineSeedJp.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
